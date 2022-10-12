39087940a1732bc25a12e7707b9ea0f7f727cd88
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-10-12 01:57:49 -0300

    added redirect to whatsapp message

--- src/components/Reserva/Reserva.jsx
+++ src/components/Reserva/Reserva.jsx
@@ -1,23 +1,61 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Reserva.css";
 
 const Reserva = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    console.log("handleSubmit ran");
+    event.preventDefault(); // 👈️ prevent page refresh
+
+    let message = `Hola%20Juli%21%20Mi%20nombre%20es%20${firstName}%20${lastName}%20y%20quisiera%20reservar%20un%20turno.`;
+    let URL = `https://wa.me/543492383044?text=` + message;
+    
+    window.open(URL, "_blank");
+
+    window.location.href = "/"
+  };
+
   return (
     <div className="turno-container">
-      <form className="turno-form">
+      <form className="turno-form" onSubmit={handleSubmit}>
         <div className="input-form">
           {/* <label htmlFor="nombre">Nombre:</label> */}
-          <input type="text" name="nombre" placeholder="Nombre" required />
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Nombre"
+            onChange={(event) => setFirstName(event.target.value)}
+            value={firstName}
+            required
+          />
         </div>
         <div className="input-form">
           {/* <label htmlFor="apellido">Apellido:</label> */}
-          <input type="text" placeholder="Apellido" required />
+          <input
+            type="text"
+            placeholder="Apellido"
+            onChange={(event) => setLastName(event.target.value)}
+            value={lastName}
+            required
+          />
         </div>
         <div className="input-form">
           {/* <label htmlFor="email">Correo electronico:</label> */}
-          <input type="email" name="email" placeholder="turno@ejemplo.com" required />
+          <input
+            type="email"
+            name="email"
+            placeholder="turno@ejemplo.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
         </div>
-        <input type="submit" value="" />
+        <button type="submit">Reservar Turno</button>
       </form>
     </div>
   );
