c050f0fcc24d113b44723d354d98ead76815aee2
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-10-13 11:23:39 -0300

    redisigning styles

--- src/components/Home/Titles/Title.jsx
+++ src/components/Home/Titles/Title.jsx
@@ -3,14 +3,12 @@ import React from "react";
 const Title = ({ text }) => {
   const separatorStyle = {
     textAlign: "center",
-    color: "hotpink",
+    color: "#f14d59",
     fontSize: "48px",
   };
   return (
     <div style={{ margin: "3rem 0rem" }}>
-      <hr />
-      <h1 style={separatorStyle}>{text}</h1>
-      <hr />
+      <h2 style={separatorStyle}>{text}</h2>
     </div>
   );
 };
