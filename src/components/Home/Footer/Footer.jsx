d9c41db60cdd4c5cf7d018d4a37a57f9743c6c61
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-10-29 16:05:31 -0300

    padding hero

--- src/components/Home/Footer/Footer.jsx
+++ src/components/Home/Footer/Footer.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <ul>
+        <li>Home</li>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Socials</li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
